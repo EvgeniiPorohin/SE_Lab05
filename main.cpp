9e3186f882800abfaa649899bff0e9d03f9dd98b
Your Name <you@example.com>
2022-01-18 01:24:58 +0500

    code v vetka

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ cout << "Enter A and B: ";
 cin >> a >> b;
 cout << "A + B = " << a + b << '\n'
      << "A - B = " << a + b << '\n'
-     << "A + B = " << a + b << '\n';
+     << "A \ B = " << a + b << '\n';
 
 
 }
